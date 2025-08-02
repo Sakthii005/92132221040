ffd7f8c141fe88748a8c711981b58643b7f03b5a
Sakthii005 <pirammasakthi42@gmail.com>
2025-08-02 12:58:15 +0530

    Final commit

--- Backend/app.js
+++ Backend/app.js
@@ -150,5 +150,5 @@ app.get('/shorturls/:shortcode', async (req, res) => {
 
 
 app.listen(8000, () => {
-    console.log('URL Shortener Microservice running at http://localhost:8000');
+    console.log('Server running at http://localhost:8000');
 });
